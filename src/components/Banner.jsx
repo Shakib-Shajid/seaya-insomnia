940f908c11dcaea877baa918fc46a533dc4a6d8e
shakib-shajid <shakibalshajid1682@gmail.com>
2024-11-28 22:35:39 +0600

    banner text updated and button added

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -44,7 +44,7 @@ const Banner = () => {
         <div>
             <section className="h-screen text-white relative overflow-hidden">
                 {/* Background gradient */}
-                <div className="absolute inset-0 bg-fixed bg-cover bg-center bg-gradient-to-r from-purple-500 to-blue-600"></div>
+                <div className="absolute inset-0 bg-fixed bg-cover bg-center bg-gradient-to-r from-[#4a00e0] to-[#8e2de2]"></div>
 
                 {/* SVG Wave positioned in the center */}
                 <div className="absolute inset-0 flex justify-center items-end">
@@ -52,7 +52,7 @@ const Banner = () => {
                 </div>              
 
 
-                <div className="absolute inset-0 bg-black opacity-50"></div>
+                {/* <div className="absolute inset-0 bg-black opacity-50"></div> */}
 
                 {/* Content container */}
                 <div className="relative h-[80%] flex flex-col justify-center items-center text-center px-4 space-y-6">
@@ -63,8 +63,9 @@ const Banner = () => {
 
                     {/* Text - Positioned lower and slightly smaller than the image */}
                     <div>
-                        <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold mb-4">Digital Native Brands</h1>
-                        <p className="text-lg md:text-xl lg:text-2xl max-w-2xl mb-6">Fashion and leisure for the near future</p>
+                        <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold mb-4">Redefining Digital Native Brands</h1>
+                        <p className="text-lg md:text-xl lg:text-2xl mb-6 text-center">Where Innovation Meets Investment.</p>
+                        <Link href="#"><button className='btn bg-[#4a00e0] hover:bg-[#4a00e0] text-white border-none rounded-full btn-lg text-lg'>Explore Opportunities</button></Link>
                     </div>
                 </div>
             </section>
